f0a1f61c97f7fda8c60e7f74db5763f40e057442
Mark Shirley <markisisme@gmail.com>
2009-07-20 13:49:09 -0700

    change extconf to be inline with swig rename

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -3,5 +3,5 @@ $libs = append_library($libs, "supc++")
 $libs = append_library($libs, "Timbl")
 
 with_cppflags("-I/usr/local/include/timbl -L/usr/local/lib") do
-  create_makefile('rtimbl')
+  create_makefile('ruby_timbl')
 end
